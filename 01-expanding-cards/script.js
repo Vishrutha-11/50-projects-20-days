a1a9b386e2f0ee0a0138ed6e947072ec62173936
Vishrutha03 <vishruthavishu297@gmail.com>
2025-12-20 12:46:26 -0800

    completed 8 projects

--- 01-expanding-cards/script.js
+++ 01-expanding-cards/script.js
@@ -1,58 +1,15 @@
-console.log("Sri Rama")
+const panels = document.querySelectorAll('.panel');
 
-
-const firstPanelElement = document.querySelector(".first")
-const secondPanelElement = document.querySelector(".second")
-const thirdPanelElement = document.querySelector(".third")
-const fourthPanelElement = document.querySelector(".fourth")
-const fifthPanelElement = document.querySelector(".fifth")
-
-firstPanelElement.addEventListener("click", ()=>{
-    console.log("Clicked first")
-    removeActiveClass();
-    firstPanelElement.classList.add("active")
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        remove()
+        panel.classList.add('active')
+    })
 })
 
-secondPanelElement.addEventListener("click", ()=>{
-    console.log("Clicked second")
-    removeActiveClass();
-    secondPanelElement.classList.add("active")
-})
-
-thirdPanelElement.addEventListener("click", ()=>{
-    console.log("Clicked third")
-    removeActiveClass();
-    thirdPanelElement.classList.add("active")
-})
-
-fourthPanelElement.addEventListener("click", ()=>{
-    console.log("Clicked fourth")
-    removeActiveClass();
-    fourthPanelElement.classList.add("active")
-})
-
-fifthPanelElement.addEventListener("click", ()=>{
-    console.log("Clicked fifth")
-    removeActiveClass();
-    fifthPanelElement.classList.add("active")
-})
-
-function removeActiveClass(){
-    const allPanelElements = document.querySelectorAll(".panel")
-    allPanelElements.forEach(ele => {
-        ele.classList.remove('active');
+function remove() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
     })
 }
 
-// const panelElements = document.querySelectorAll(".panel")
-
-// console.log("panelElements ====> ", panelElements)
-
-// for (let i = 0; i < panelElements.length; i++) {
-//     const element = panelElements[i];
-//     console.log("element =====> ", element);
-    
-//     element.addEventListener("click", ()=>{
-//         console.log("clicked element =====> ", element)
-//     })
-// }
---
+++ 08-form wave/script.js
@@ -0,0 +1,20 @@
+
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
+
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+
+})
+
+    .map((letter, idx) => `<span style="transistion-delay:${idx, 50}ms>${letter}</span>`)
