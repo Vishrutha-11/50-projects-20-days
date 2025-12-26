65b3e3d379eeeeb37289c5f3cc286d0faadfe876
Vishrutha03 <vishruthavishu297@gmail.com>
2025-12-26 00:00:05 -0800

    Completed

---
+++ 12-FAQ collapse/index.js
@@ -0,0 +1,19 @@
+// const toggles = document.querySelectorAll(".faq-toggle")
+
+// console.log("yjhv", toggles)
+
+// toggles.forEach(toggle => {
+//     toggle.addEventListener("click", () => {
+//         toggle.parentNode.classList.toggle("active")
+//     })
+// })
+
+const toggles = document.querySelectorAll(".faq-toggle")
+console.log("jeckn", toggles)
+
+
+toggles.forEach(toggle => {
+    toggle.addEventListener("click", () => {
+        toggle.parentNode.classList.toggle("active")
+    })
+})
